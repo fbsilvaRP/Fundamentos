2d200fbbce79b25d60e3362eeaf4382433f03bf2
Felipe Braga <febragasilva@outlook.com>
2026-04-09 09:09:48 -0300

    Produção do exercício 1

---
+++ C/Capítulo 5/Exercícios Resolvidos/EX01 - Exercício resolvido.cpp
@@ -0,0 +1,21 @@
+//Programa que determina o salario atual de um funcionario 
+#include <stdio.h>
+int main()
+{
+	//Definicao do salario inicial 
+	float salarioInit = 1000;
+	
+	//Salario apos 2006 - Aumento de 1,5%
+	salarioInit = salarioInit * (1 + 0.015);
+	
+	//Taxa unitaria de aumento
+	float taxa = 0.015; 
+	
+	//Salario a partir de 2007:
+		//A taxa de aumento salario corresponde ao dobro do percentual do ano anterior
+		
+		
+	
+	 
+	
+}
