0a8e7224d67b206f473cff3e50fdb9beb5671c13
Felipe Braga <febragasilva@outlook.com>
2026-04-09 08:38:56 -0300

    Edição no exercício 10

--- C/Capítulo 4/Exercícios Resolvidos/EX10 - Exercício resolvido.cpp
+++ C/Capítulo 4/Exercícios Resolvidos/EX10 - Exercício resolvido.cpp
@@ -35,6 +35,51 @@ int main()
 	printf("\nSegunda data coletada: %d/%d/%d", dia2, mes2, ano2);
 	// --------------------------------------------------------------------------------------------- 
 	//Definindo a data cronologicamente maior - como definir?
+	//lógica utilizada: 
+		// * Verificador 0 significa que a data 1 eh maior
+		// * Verificador 1 significa que a data 2 eh maior 
+
+	//Começa verificando qual dos anos é maior
+	if(ano1 > ano2)
+		verificador = 0;
+	else
+	{
+		if(ano2 > ano1)
+			verificador = 1;
+		
+		//Se os anos forem iguais, o criterio de comparacao utilizado sera o mes
+		if(ano1 == ano2)
+		{
+			if(mes1 > mes2)
+				verificador = 0;
+				
+			else
+			{
+				if(mes2 > mes1)
+					verificador = 1;
+				
+				//Se os meses forem iguais, o criterio de comparacao utilizado sera o dia
+				if(mes1 == mes2)
+				{
+					if(dia1 > dia2)
+						verificador = 0;
+					
+					if(dia2 > dia1)
+						verificador = 1;
+					
+					else
+						printf("\nAs duas datas sao iguais.");	
+						return 0; //Encerra o programa sem passar pela verificação, caso as duas datas sejam iguais
+				}
+			}
+		}
+	}
 	
+	if(verificador == 0)
+		printf("\nA primeira data digitada eh maior");
+	else
+		printf("\nA segunda data eh maior");
+	
+	return 0; 
 }
 	
