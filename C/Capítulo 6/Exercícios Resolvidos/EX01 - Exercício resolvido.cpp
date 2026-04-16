999bf6fc00d46b7fe1f7caddcbf69f305876618a
Felipe Braga <febragasilva@outlook.com>
2026-04-16 09:32:57 -0300

    Edições...

---
+++ C/Capítulo 6/Exercícios Resolvidos/EX01 - Exercício resolvido.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define tam 9
+int main()
+{
+	//Definindo o vetor de tamanho constante
+	int vetor[tam]; 
+	
+	//Variável para receber o valor do usuário e inserir no vetor
+	int valor = 0; 
+	
+	//Preenchendo o vetor com valores decididos pelo usuário
+	for(int i = 0; i <= tam; i++)
+	{
+		
+	}
+	
+}
